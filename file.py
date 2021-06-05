3bbc30b8ceb7e0cc0c3ddc2568100bc8fa71d104
Shaik Khaja Hafeezuddin <hafeezskhbablu@gmail.com>
2021-06-06 00:08:55 +0530

    Add files via upload
    CONCEPT OF MODULES EXPLAINED SIMPLE

---
+++ file.py
@@ -0,0 +1,9 @@
+
+
+import mod1
+print(mod1.add(2,4))  #importing a module mod1 and usinf function add
+
+a=mod1.power(2,3)
+print(a)
+
+mod1.uppercase()
---
+++ mod1.py
@@ -0,0 +1,18 @@
+import mod1
+
+def add(a,b):
+    c=a+b
+    print(c)
+    
+def power(i,j):
+    k=i**j
+    print(k)
+
+def uppercase():
+    a=input("Enter the data:")
+    if a.isupper():
+        a=a.lower()
+        print(a)
+
+
+    
